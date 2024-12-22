a600ed5e56e1042f9bb939bd7ee2f4a82003544f
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-12-22 23:08:37 +0530

    spring boot 3.4.1 upgrade

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ Start Service:
 ```bash
 mvn spring-boot:run
 ```
+
+### OUTPUT
+
+```
+Generating XML log file ...dlog.xml
+KEY: XMLLog VALUE: com.example.api.XMLLog@6419a0e1
+KEY: jsonLog VALUE: com.example.api.JSONLog@280d4882
+KEY: xmlLog VALUE: com.example.api.XMLLog@44af588b
+```
--- src/main/java/com/example/SpringBootCustomAutoConfig.java
+++ src/main/java/com/example/SpringBootCustomAutoConfig.java
@@ -4,20 +4,24 @@ import com.example.api.LogApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootCustomAutoConfig {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCustomAutoConfig.class, args);
-	}
-	
-	@Bean
-	public CommandLineRunner commandLineRunner(LogApi logapi) {
-		
-		return args -> { logapi.generate("dlog"); };
-		
-	}
-	
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootCustomAutoConfig.class, args);
+        applicationContext.getBeansOfType(LogApi.class).forEach((k, v) -> System.out.println("KEY: " + k + " VALUE: " + v));
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(LogApi logapi) {
+
+        return args -> {
+            logapi.generate("dlog");
+        };
+
+    }
+
 }
